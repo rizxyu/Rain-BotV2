949d176639fd86fc370ea7afd7d800083b3375c2
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-24 19:43:24 +0700

    Merapihkan

--- plugins/baileys-react.js
+++ plugins/baileys-react.js
@@ -1,20 +1,15 @@
-import { 
-WAProto,
-generateMessageID
-} from "@adiwajshing/baileys"
+import { WAProto, generateMessageID } from "@adiwajshing/baileys"
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+let who = m.quoted
 if (!args[0]) throw `example: ${usedPrefix +command} 😎`
+if (!who) throw `Tag someone`
 
 const react = new WAProto.ReactionMessage.create({})
         react.key = { remoteJid: m.chat, fromMe: true, id: m.quoted.id, }
         react.text = args[0]
-        react.senderTimestampMs = {
-            low: 12345678,
-            high: 0,
-            unsigned: false
-        }
-
+        react.senderTimestampMs = { low: 12345678, high: 0, unsigned: false }
+        
          conn.relayMessage(m.chat, { reactionMessage: react }, { messageId: generateMessageID() })
          m.reply(m.quoted.id)
 }
