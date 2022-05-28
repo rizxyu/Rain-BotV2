dee0f634baa7889f98e5a314545ca18eb2e2433c
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-28 11:20:48 +0700

    Change Tiktok/Fix

--- plugins/downloader-tiktok2.js
+++ plugins/downloader-tiktok2.js
@@ -5,13 +5,15 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
     const { author: { nickname }, video, description } = await tiktokdl(args[0])
     const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `*TIKTOK DOWNLOADERV2*
 *Nickname:* ${nickname}
 *Description:* ${description}
+
+_SUPPORT OWNER DGN DONASI_
 `.trim(), m)
 }
-handler.help = ['tiktok2'].map(v => v + ' <url>')
+handler.help = ['tiktok2', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok2)?)$/i
+handler.command = /^(tik(tok2)?(tok)?(dl)?)$/i
 
 export default handler
