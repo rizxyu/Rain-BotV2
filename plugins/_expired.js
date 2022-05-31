06f183c75592502841b7baf5f226616b39334187
Rizky <88314302+Rizxyu@users.noreply.github.com>
2022-05-31 18:30:17 +0700

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -5,7 +5,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, 'Bye🖐 bot akan left Silahkan hubungi owner untuk perpanjangan bot!')
+        await this.reply(m.chat, 'Bye🖐 bot akan left , Silahkan hubungi owner untuk perpanjangan bot!.')
         await this.groupLeave(m.chat)
         chats.expired = null
     }
