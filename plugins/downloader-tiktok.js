15c4b858f3f62358d8e96e98a8664aaa0d2a5c6d
J <88314302+carlxjoe@users.noreply.github.com>
2022-10-06 17:38:02 +0700

    Fix tiktok eror

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,56 +1,26 @@
-import axios from "axios"
-import cheerio from "cheerio"
-import FormData from "form-data"
-
+import { savefrom } from "../scraper/savefrom.js"
+import { tiktokdlv3 } from "@bochilteam/scraper"
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
 //
-    let res = await axios("https://musicaldown.com/id")
-let token = []
-let $ = cheerio.load(res.data)
-$("form > div > div > input").each(function() {
-  let value = $(this).val()
-  let name = $(this).attr("name")
-  if (!value) value = args[0]
-  token.push(name, value)
-})
-let form = new FormData
-form.append(token[0], token[1])
-form.append(token[2], token[3])
-form.append(token[4], token[5])
-let resu = await axios({
-  url: "https://musicaldown.com/id/download",
-  method: "post",
-  data: form,
-  headers: {
-    ...form.getHeaders(),
-    cookie: res.headers["set-cookie"]
-  }
-})
-let resul = await axios({
-  url: "https://musicaldown.com/id/mp3",
-  method: "post",
-  headers: {
-    cookie: res.headers["set-cookie"]
-  }
-})
-let $$ = cheerio.load(resu.data)
-let $$$ = cheerio.load(resul.data)
-let result = {
-  video: $$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(5)").attr("href"),
-  audio: $$$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(5)").attr("href"),
-  video_original: $$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(9)").attr("href"),
-  audio_original: $$$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(9)").attr("href"),
-  preview: $$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l4 > img").attr("src")
-}
-    const url = result.video || result.video_original
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `*Tiktok Downloader*
+    let res = ( await savefrom(args[0])).url[0]
+    let result = await savefrom(args[0])
+   
+   let { title, source, duration } = result.meta
+   let { url } = res
+   if (!url) throw 'Can\'t download video!'
+   await conn.reply(m.chat, wait, m)
+   await  conn.sendFile(m.chat, url, 'tiktok.mp4', `*${command} Downloader*
+   
+${title ? "*Title:* "+ title : "Untitled"}
+${source ? "*Source:* " + source : "unsource"}
+${duration ? "*Duration:* " + duration : "null"}
 
 Support Me ${trakteer}`.trim(), m)
 }
-handler.help = ['tiktok', 'tiktok2', 'tiktokdl'].map(v => v + ' <url>')
+handler.help = ['tiktok', 'tiktok2', 'tiktokdl', "savefrom"].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tik(tok2)?(tok)?(dl)?(nowm)?)$/i
+handler.command = /^(tik(tok2)?(tok)?(dl)?(nowm)?|save(from)?)$/i
+handler.limit = true
 
 export default handler
