d4f2ebc3c0aa5409f79daffc47793ffb3264e34f
J <88314302+carlxjoe@users.noreply.github.com>
2022-10-11 12:13:29 +0700

    Update _allfunct.js

--- plugins/_allfunct.js
+++ plugins/_allfunct.js
@@ -1,118 +1,115 @@
 
-//MODULE
 import db from "../lib/database.js";
 import axios from "axios"
 import cheerio from "cheerio"
 import FormData from "form-data"
-import {
-  facebookdlv2,
-  facebookdl,
-} from "@bochilteam/scraper";
-import moment from 'moment-timezone'
+import instagramGetUrl from 'instagram-url-direct'
 
 //Auto respon other
-export async function all(m) {
-  if (m.chat.endsWith('status@broadcast')) {
-        console.log('YOW!')
-    }
-    let { isBanned } = db.data.chats[m.chat]
-    let { banned } = db.data.users[m.sender]
+export async function all(m, { isPrems, isOwner}) {
+    
     let setting = db.data.settings[this.user.jid]
     let user = db.data.users[m.sender]
-    //Responder
-    if (m.isGroup) {
-      if (m.mentionedJid.includes(this.user.jid)) {
-        await this.sendButton(m.chat, isBanned ? "Grup Ini Diban Oleh owner" : banned ? "Anda telah dibanned" : "Bot Status: Online", author, null, [[isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
-            isBanned ? '.unban' : banned ? '.owner' : '.menu'
-                ]], m)
+    let chat
+    //AUTO SET STATUS
+   let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    this.setStatus(`Ketik /help untuk melihat list fitur | Bot Aktif Selama: ${uptime}   | 🪀Whatsapp aktif: ${muptime} - IG: rizxyux`)
+    //GC INVIT
+    if ((m.mtype === 'groupInvitem.senderssage' || m.text.startsWith('https://chat') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
+      if (isOwner && isPrems ) {
+        let res = await conn.groupAcceptInvite(m.text)
+        chat = db.data.chats[m.res]
+        chat.expired += +new Date() + 7 * 1000 * 60 * 60 * 24
+         let caption = `${conn.getName(conn.user.jid)} Adalah Bot whatsapp Yang dibangun dengan NodeJs, Grup Telah Diijinkan oleh @${m.sender.split("@")[0]}${expired ? " selama " + expired + " hari" : ""}. Ketik /menu Untuk melihat daftar fitur\nJangan lupa upgrade ke premium untuk mengakses semua fitur tanpa batas!\n\n*Note:* _Jangan Spam Bot_`
+        this.sendButton(res, caption, author, [["Menu",".menu"]], m, { mentions: this.parseMention(caption) })
+      } else {
+    this.reply(m.chat,"```Auto Responder```" + "\n" + "Ketik *.join linkgrup* - untuk permintaan masukan bot kedalam grup yg diminta atau hubungi developer",m)
       }
     }
-    //Kontak
-    global.kontak2 = [
-         [owner[0], await this.getName(owner[0] + '@s.whatsapp.net'), 'who', 'r@gmail.com', true],
-         [owner[1], await this.getName(owner[1] + '@s.whatsapp.net'), 'Whos', 'rizkynursanto48@gmail.com', true], // Kalo mau di tambah tinggal copy 1baris ini di tempel di bawahnya trs di edit dikit!
-        ]
-        //Ucapan
-        global.ucapan = ucapan()
+    /**
+     * 
+     * Doc Buat Fake reply
+     * 
+    **/
+    global.doc = pickRandom(["application/vnd.ms-excel", "application/vnd.openxmlformats-officedocum.sendernt.presentationml.presentation", "application/msword", "application/pdf"])
+		
+		/**
+		 *
+		 * 
+		 * Module
+		 * 
+		**/
+		global.nowS = "Season 3 🎃- Hellowen 9 Sep s/d 9 November"
+    global.fetch = (await import('node-fetch')).default
+    global.skrep = (await import("@bochilteam/scraper"));
+    global.skrep2 = ( await import('../lib/scrape.js'))
+    global.axios = axios
+    global.cheerio = cheerio
+    global.foda = FormData
+    global.canva = ( await import("canvacord")).default
+    global.fs = (await import("fs")).promises
+    global.fsd = (await import("fs")).default
+     global.user = db.data.users[m.sender]
+     global.changelog = `*V3.8.6.09 (STABIL) - 7 Oktober 9:54 Am*
+     Penambahan Film Bioskop Now
+     Penambahan Zippyshare
+     Penambahan Berita Merdeka
+     Penambahan fitur searching Happymod
+     Penambahan Fitur Ocr image to text
+     Penambahan SendPoll
+     Fix Eror Togif dan penambahan Tovideo
+     Penambahan Ramalan Jodoh
+     Penambahan set status khusus pemilik
+     Fix Eror Tiktok (Memakai limit)
+     Penambahan Savefrom (Memakai limit)`
+    // global.m.chat = m.chat
+    // global.zyz = m.chat
+    global.bendera = ("🇦🇨🇦🇩🇦🇪🇦🇫🇦🇬🇦🇮🇦🇱🇦🇲🇦🇴🇦🇶🇦🇷🇦🇸🇦🇹🇦🇺🇦🇼🇦🇽🇦🇿🇧🇦🇧🇧🇧🇩🇧🇪🇧🇫🇧🇬🇧🇭🇧🇮🇧🇯🇧🇱🇧🇲🇧🇳🇧🇴🇧🇶🇧🇷🇧🇸🇧🇹🇧🇻🇧🇼🇧🇾🇧🇿🇨🇦🇨🇨🇨🇩🇨🇫🇨🇬🇨🇭🇨🇮🇨🇰🇨🇱🇨🇲🇨🇳🇨🇴🇨🇵🇨🇷🇨🇺🇨🇻🇨🇼🇨🇽🇨🇾🇨🇿🇩🇪🇩🇬🇩🇯🇩🇰🇩🇲🇩🇴🇩🇿🇪🇦🇪🇨🇪🇪🇪🇬🇪🇭🇪🇷🇪🇸🇪🇹🇪🇺🇫🇮🇫🇯🇫🇰🇫🇲🇫🇴🇫🇷🇬🇦🇬🇧🇬🇩🇬🇪🇬🇫🇬🇬🇬🇭🇬🇮🇬🇱🇬🇲🇬🇳🇬🇵🇬🇶🇬🇷🇬🇸🇬🇹🇬🇺🇬🇼🇬🇾🇭🇰🇭🇳🇭🇷🇭🇹🇭🇺🇮🇨🇮🇩🇮🇪🇮🇱🇮🇲🇮🇳🇮🇴🇮🇶🇮🇷🇮🇸🇮🇹🇯🇪🇯🇲🇯🇵🇰🇪🇰🇬🇰🇭🇰🇮🇰🇲🇰🇳🇰🇵🇰🇷🇰🇼🇰🇾🇰🇿🇱🇦🇱🇧🇱🇨🇱🇮🇱🇰🇱🇷🇱🇸🇱🇹🇱🇺🇱🇻🇱🇾🇲🇦🇲🇨🇲🇩🇲🇪🇲🇫🇲🇬🇲🇭🇲🇰🇲🇱🇲🇲🇲🇳🇲🇴🇲🇵🇲🇶🇲🇷🇲🇸🇲🇹🇲🇺🇲🇻🇲🇼🇲🇽🇲🇾🇲🇿🇳🇦🇳🇨🇳🇪🇳🇫🇳🇬🇳🇮🇳🇱🇳🇴🇳🇵🇳🇷🇳🇺🇳🇿🇴🇲🇵🇦🇵🇪🇵🇫🇵🇬🇵🇭🇵🇰🇵🇱🇵🇲🇵🇳🇵🇷🇵🇸🇵🇹🇵🇼🇵🇾🇶🇦🇷🇪🇷🇴🇷🇸🇷🇺🇷🇼🇸🇦🇸🇨🇸🇩🇸🇪🇸🇬🇸🇭🇸🇮🇸🇯🇸🇰🇸🇱🇸🇲🇸🇳🇸🇴🇸🇷🇸🇸🇸🇹🇸🇻🇸🇽🇸🇾🇸🇿🇹🇦🇹🇨🇹🇩🇹🇫🇹🇬🇹🇭🇹🇯🇹🇰🇹🇱🇹🇲🇹🇳🇹🇴🇹🇷🇹🇹🇹🇻🇹🇼🇹🇿🇺🇦🇺🇬🇺🇲🇺🇳🇺🇸🇺🇾🇺🇿🇻🇦🇻🇨🇻🇪🇻🇬🇻🇮🇻🇳🇻🇺🇼🇫🇼🇸🇽🇰🇾🇹🇿🇦🇿🇲🇿🇼🏴󠁧󠁢󠁥󠁮󠁧󠁿🏴󠁧󠁢󠁳󠁣󠁴󠁿🏴󠁧󠁢󠁷󠁬󠁳󠁿")
+    
+     global.db = ( await import("../lib/database.js"))
+     global.wrong = "*Salah*"
+     global.fact = "*Benar*"
+     global.eror = "Terjadi Kegagalan fatal pada kode"
+     global.wait = "Sedang diproses... Tunggu beberapa *Menit* atau *Detik*"
+     global.
+     global.fail = "Gagal Mendapatkan data
+     global.cpt = ""//Mempermudanh
+     global.beta = "FITUR INI MASIH DALAM PENGEMBANGAN, JIKA ADA EROR HUBUNGI DEVELOPER BOT"
+     
+    
+        
 }
 
 
 
-//Auto downloader
-export async function before(m, { isAdmin, isBotAdmin }) {
-  let chat = db.data.chats[m.chat]
-    let user = db.data.users[m.sender]
-    let set = db.data.settings[this.user.jid]
-    let teks = m.text //Simple
-    if (m.chat.endsWith('broadcast')) return
-    if (chat.isBanned || user.banned || !chat.download || m.isBaileys) return
-    //hmmm?
-    try {
-      if (/https?:\/\/(www\.|v(t|m)\.|t\.)?tiktok\.com/i.test(teks)) {
-        let res = await axios("https://musicaldown.com/id")
-let token = []
-let $ = cheerio.load(res.data)
-$("form > div > div > input").each(function() {
-  let value = $(this).val()
-  let name = $(this).attr("name")
-  if (!value) value = teks
-  token.push(name, value)
-})
-let form = new FormData
-form.append(token[0], token[1])
-form.append(token[2], token[3])
-form.append(token[4], token[5])
-let resu = await axios({
-  url: "https://musicaldown.com/id/download",
-  method: "post",
-  data: form,
-  headers: {
-    ...form.getHeaders(),
-    cookie: res.headers["set-cookie"]
-  }
-})
-let resul = await axios({
-  url: "https://musicaldown.com/id/mp3",
-  method: "post",
-  headers: {
-    cookie: res.headers["set-cookie"]
-  }
-})
-let $$ = cheerio.load(resu.data)
-let $$$ = cheerio.load(resul.data)
-let result = {
-  video: $$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(5)").attr("href"),
-  audio: $$$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(5)").attr("href"),
-  video_original: $$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(9)").attr("href"),
-  audio_original: $$$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l8 > a:nth-child(9)").attr("href"),
-  preview: $$("body > div.welcome.section > div > div:nth-child(2) > div.col.s12.l4 > img").attr("src")
-}
-conn.sendFile(m.chat, result.video || result.video_original, "tiktok.mp4", `*Tiktok AutoDownloader*`, m)
-      }
+
+  
       
-    } catch (e){
-      m.reply("Eror")
-    }
-}
 
-function ucapan() {
-    const time = moment.tz('Asia/Jakarta').format('HH')
-    let res = "Selamat malam 🌙"
-    if (time >= 4) {
-        res = "Selamat pagi 🌄"
-    }
-    if (time > 10) {
-        res = "Selamat siang ☀️"
-    }
-    if (time >= 15) {
-        res = "Selamat sore 🌅"
-    }
-    if (time >= 18) {
-        res = "Selamat malam 🌙"
-    }
-    return res
-}
+  
+
+
+
+
+
+
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
+}
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
