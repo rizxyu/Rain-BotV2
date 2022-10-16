3d2376be66b96164a11b13250a1964392e33026d
J <88314302+carlxjoe@users.noreply.github.com>
2022-10-16 07:17:12 +0700

    Penambahan apkdl

---
+++ scraper/apkdl.js
@@ -0,0 +1,17 @@
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+
+
+async function apkDl(url) {
+	let res = await fetch('https://apk.support/gapi/index.php', {
+		method: 'post',
+		body: new URLSearchParams(Object.entries({ x: 'downapk', t: 1, google_id: url, device_id: '', language: 'en-US', dpi: 480, gl: 'SUQ=', model: '', hl: 'en', de_av: '', aav: '', android_ver: 5.1, tbi: 'arm64-v8a', country: 0, gc: undefined }))
+	})
+	let $ = cheerio.load(await res.text())
+	let fileName = $('div.browser > div.dvContents > ul > li > a').text().trim().split(' ')[0]
+	let download = $('div.browser > div.dvContents > ul > li > a').attr('href')
+	if (!download) throw 'Can\'t download the apk!'
+	let mimetype = (await fetch(download, { method: 'head' })).headers.get('content-type')
+	return { fileName, mimetype, download }
+}
+export default handler
